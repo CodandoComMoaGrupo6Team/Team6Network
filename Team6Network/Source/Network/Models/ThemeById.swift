d9df24c9d92ab18ee36536b4617b1346741b732f
wmilani <milani.weslley@live.com>
2023-09-04 23:39:27 -0300

    criacao das models e estruturacao das pastas

--- Team6Network/Source/Network/Models/ThemeById.swift
+++ Team6Network/Source/Network/Models/ThemeById.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+// MARK: - Welcome
+struct ThemeByIdModel: Codable {
+    let resultado2: Result
+    let resultJSON: String
+    let isSucess: Bool
+    let message: String
+    let tester: Exception // Alterar o nome da variavel para algo bonito e funcional 
+    let stackTrace: String
+}
+
+// MARK: - Exception
+struct tester: Codable {
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let name: String
+    let id: Int
+    let isInativo: Bool
+    let creationDate, changeDate, uid, uidFirebase: String
+    let isChanged: Bool
+}
--- Team6Network/Source/Network/Models/ThemeComponenteById.swift
+++ Team6Network/Source/Network/Models/ThemeComponenteById.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct ThemeComponenteById: Codable {
+    let resultado: Result
+    let resultJSON: String
+    let isSucess: Bool
+    let message: String
+    let teste1: Exception // alterar o nome da variavel para algo que faca sentido, coloquei esse so para sumir os warnings
+    let stackTrace: String
+}
+
+// MARK: - Exception
+struct teste1: Codable { // alterar o nome da classe 
+}
+
+// MARK: - Result
+struct Resultado: Codable {
+    let themeID: Int
+    let theme: Theme
+    let name, foregroundColor, backgroundColor, fontName: String
+    let fontSize: Int
+    let style: String
+    let id: Int
+    let isInativo: Bool
+    let creationDate, changeDate, uid, uidFirebase: String
+    let isChanged: Bool
+}
+
+// MARK: - Theme
+struct Theme:Codable {
+    let name: String
+    let id: Int
+    let isInativo: Bool
+    let creationDate, changeDate, uid, uidFirebase: String
+    let isChanged: Bool
+}
